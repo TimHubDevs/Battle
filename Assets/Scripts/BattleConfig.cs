59d55f4b8fe5475fafde97ca05cc350210aecdb9
Tim <devsspacegame@gmail.com>
2022-01-31 16:45:09 +0200

    First commit first step in project

---
+++ Assets/Scripts/BattleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BattleConfig", order = 2)]
+public class BattleConfig : ScriptableObject
+{
+    public int countCloser;
+    public int countFarer;
+    public BattlerIndex[] BattlerIndicces = new BattlerIndex[6];
+    public List<BattlerIndex> BattlerIndices = new List<BattlerIndex>();
+    private void OnValidate()
+    {
+        if (countCloser + countFarer > 6)
+        {
+            countCloser = 0;
+            countFarer = 0;
+            BattlerIndices.Clear();
+            BattlerIndicces = new BattlerIndex[6];
+            return;
+        }
+        else if (countCloser > 0)
+        {
+            for (int i = 0; i < countCloser; i++)
+            {
+                BattlerIndex battlerIndex = new BattlerIndex();
+                battlerIndex.TypeFighter = TypeFighter.CLOSER;
+                battlerIndex.indexPosition = i;
+                BattlerIndices.Add(battlerIndex);
+                BattlerIndicces[i] = battlerIndex;
+            }
+        }
+        else if (countFarer > 0)
+        {
+            for (int i = 0 ; i < countFarer; i++)
+            {
+                BattlerIndex battlerIndex = new BattlerIndex();
+                battlerIndex.TypeFighter = TypeFighter.FARER;
+                battlerIndex.indexPosition = i + BattlerIndices.Count;
+                BattlerIndices.Add(battlerIndex);
+                BattlerIndicces[i+BattlerIndicces.Length] = battlerIndex;
+            }
+        }
+    }
+}
+
+public class BattlerIndex
+{
+    public TypeFighter TypeFighter;
+    public int indexPosition;
+}
---
+++ Assets/Scripts/SOFighter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SOFighter", order = 1)]
+public class SOFighter : ScriptableObject
+{
+    public string fighterName;
+    public TypeFighter typeFighter;
+    public GameObject prefab;
+    public int health;
+    public int damage;
+}
+
+public enum TypeFighter
+{
+    CLOSER,
+    FARER
+}
